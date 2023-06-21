081e19f57f292e076b818fc8880a6eaed9882ac6
Tocci997 <duet997@gmail.com>
2023-06-21 07:58:26 -0700

    Functions and nested loops

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
  * _isalpha - checks for alphabetic character
- * @c: a character in ASCII code
+ * @c: the ASCII character to be checked
  * Return: 1 indicates lowercase or uppercase character and 1 for otherwise
  */
 int _isalpha(int c)
 {
-	for ((c >= 65 && c <= 90) || (c >= 97 && c <= 122);)
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 	{
 		return (1);
 	}
-	if else
+	else
 	{
 		return (0);
 	}
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,7 +11,7 @@ int print_last_digit(int n)
 	int l;
 
 	l = n % 10;
-	for (n < 0)
+	if (n < 0)
 	{
 		_putchar(-l + 48);
 		return (-l);
